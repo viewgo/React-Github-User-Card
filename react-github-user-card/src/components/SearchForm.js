fc198bf156062e0637a26a7a345f475b1bdbeb76
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-10-29 15:57:33 -0500

    mvp + search stretch

--- react-github-user-card/src/components/SearchForm.js
+++ react-github-user-card/src/components/SearchForm.js
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
+   
     container: {
       display: 'flex',
       flexWrap: 'wrap',
